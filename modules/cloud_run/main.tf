521594b18168f5d9234c10a08a2b8e4905f04ba7
agusbroccolli <agustina@nubiral.com>
2021-09-06 18:01:05 -0300

    change suscription for topic

--- modules/cloud_run/main.tf
+++ modules/cloud_run/main.tf
@@ -18,14 +18,12 @@ module "cloud_run" {
   version = "~> 0.1.1"
 
   # Required variables
-  service_name           = data.google_pubsub_subscription.default.message
+  service_name           = data.google_pubsub_topic.my-pubsub-topic.message.data
   project_id             = "crx-prod-btsi"
   location               = "us-central1"
   image                  = "gcr.io/crx-prod-btsi/helloreact:latest"
 }
 
-data "google_pubsub_subscription" "default" {
-  name  = "gcb-pubsub-trigger"
-  topic = "myRunTopic"
+data "google_pubsub_topic" "my-pubsub-topic" {
+  name = "myRunTopic"
 }
-
