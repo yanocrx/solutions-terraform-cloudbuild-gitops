df596e7d706ba0f4eff66b853c4be5960b3bef25
agusbroccolli <agustina@nubiral.com>
2021-09-06 15:28:01 -0300

    test new service

---
+++ modules/cloud_run/main.tf
@@ -0,0 +1,25 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+module "cloud_run" {
+  source  = "GoogleCloudPlatform/cloud-run/google"
+  version = "~> 0.1.1"
+
+  # Required variables
+  service_name           = "test1"
+  project_id             = "${var.project}"
+  location               = "us-central1"
+  image                  = "gcr.io/crx-prod-btsi/helloreact:latest"
+}
